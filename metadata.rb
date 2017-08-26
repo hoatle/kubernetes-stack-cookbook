beae5694edff75dd7e6f409fa8f176643f1a534a
hoatle <hoatle@teracy.com>
2017-08-26 08:43:09 +0700

    @ #37 | cookstyle fixed

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description 'Installs/Configures kubernetes'
 long_description 'Installs/Configures kubernetes stack'
 version '0.2.0-SNAPSHOT'
 
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
